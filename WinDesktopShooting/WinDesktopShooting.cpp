678f54f2e5a2393841d7a15ce3ef2cdcc644c8d4
EdwardSoung <allegoria88@gmail.com>
2025-09-29 16:33:36 +0900

    그림그리기
    집 + 꾸미기
    집은 방향키 누르면 이동

--- WinDesktopShooting/WinDesktopShooting.cpp
+++ WinDesktopShooting/WinDesktopShooting.cpp
@@ -5,6 +5,7 @@
 #include "WinDesktopShooting.h"
 
 #include <crtdbg.h>
+#include <string>
 #define _CRTDBG_MAP_ALLOC
 #define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
 
@@ -15,6 +16,33 @@ HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
+int KEY_DOWN = 0;
+Gdiplus::Point HouseStartPoint(0, 0);
+const int WindowHeight = 300;
+const int WindowWidth = 400;
+
+const int RoofWidth = 160;
+const int RoofHeight = 60;
+
+const int WallWidth = 120;
+const int WallHeight = 80;
+
+constexpr UINT WM_APP_KEYUP_WITH_DATA = WM_APP + 1;
+
+struct KeyUpPayload {
+    UINT vk;      // 어떤 키였는지
+    int  tag;     // 원하는 부가 데이터
+    // 필요한 만큼 필드 추가 (동일 프로세스 내 포인터 OK)
+};
+
+void OnKeyUpForward(HWND hwnd, UINT vk, int tag) {
+    auto* payload = new KeyUpPayload{ vk, tag };
+    PostMessage(hwnd, WM_APP_KEYUP_WITH_DATA,
+        static_cast<WPARAM>(vk),
+        reinterpret_cast<LPARAM>(payload));
+}
+
+int TitleBarHeight = GetSystemMetrics(SM_CYCAPTION);
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -114,7 +142,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    HWND hWnd = CreateWindowW(szWindowClass, L"2D Shooting for GDI+"/*szTitle*/,
        WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME,      //WS_MAXIMIZEBOX : 최대화 버튼 , WS_THICKFRAME 창 크기조절 선택
        200/*CW_USEDEFAULT*/, 100/*0*/,  //시작 좌표
-       400/*CW_USEDEFAULT*/, 300/*0*/,  //사이즈
+       WindowWidth/*CW_USEDEFAULT*/, WindowHeight/*0*/,  //사이즈
        nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
@@ -140,8 +168,21 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    int Limit = 0;
     switch (message)
     {
+    //case WM_APP_KEYUP_WITH_DATA: {
+    //    auto* p = reinterpret_cast<KeyUpPayload*>(lParam);
+    //
+    //    switch (p->vk)
+    //    {
+    //    case VK_UP:
+    //        OutputDebugStringW(L"KEY UP!!\n");
+    //        break;
+    //    }
+    //    delete p; // 소유권/수명 관리 확실히
+    //    return 0;
+    //}
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -168,34 +209,49 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
             Gdiplus::SolidBrush RedBrush(Gdiplus::Color(255, 255, 0, 0));
             Gdiplus::SolidBrush BlueBrush(Gdiplus::Color(255, 0, 0, 255));
+            Gdiplus::SolidBrush WhiteBrush(Gdiplus::Color(255, 255, 255, 255));
+            Gdiplus::SolidBrush GreenBrush(Gdiplus::Color(255, 100, 255, 100));
+            Gdiplus::SolidBrush BrownBrush(Gdiplus::Color(255, 165, 42, 42));
+            Gdiplus::SolidBrush LightBrownBrush(Gdiplus::Color(255, 77, 59, 37));
+            Gdiplus::SolidBrush SkyBlueBrush(Gdiplus::Color(255, 173, 216, 230));
             Gdiplus::Pen BlackPen(Gdiplus::Color(255, 0, 0, 0));
-            
-            GraphicsInstance.FillRectangle(&RedBrush, 0, 0, 60, 120);
-            //파란색 원
-            GraphicsInstance.FillEllipse(&BlueBrush, 80, 0, 80, 80);
-            //기타 도형
-            GraphicsInstance.FillPie(&RedBrush, 0, 140, 80, 80, Gdiplus::REAL(270), Gdiplus::REAL(-180));
+            Gdiplus::Pen WhitePen(Gdiplus::Color(255, 255, 255, 255));
 
+
+            GraphicsInstance.FillRectangle(&SkyBlueBrush, 0, 0, WindowWidth, WindowHeight);
+            GraphicsInstance.FillEllipse(&LightBrownBrush, -(WindowWidth / 2), WindowHeight / 2 + 30, WindowWidth * 2, WindowHeight);
+            
+            //GraphicsInstance.FillRectangle(&RedBrush, 0, 0, 60, 120);
+            ////파란색 원
+            //GraphicsInstance.FillEllipse(&BlueBrush, 80, 0, 80, 80);
+            ////기타 도형
+            //GraphicsInstance.FillPie(&RedBrush, 0, 140, 80, 80, Gdiplus::REAL(270), Gdiplus::REAL(-180));
+
+            GraphicsInstance.FillRectangle(&BrownBrush, 200, 120, 20, 80);
+            GraphicsInstance.FillEllipse(&GreenBrush, 200, 40, 20, 40);
+            GraphicsInstance.FillEllipse(&GreenBrush, 190, 60, 40, 50);
+            GraphicsInstance.FillEllipse(&GreenBrush, 180, 80, 60, 80);
+
+            Gdiplus::Rect WallRect(HouseStartPoint.X + (RoofWidth - WallWidth) / 2, HouseStartPoint.Y + RoofHeight, WallWidth, WallHeight);
+            GraphicsInstance.FillRectangle(&WhiteBrush, WallRect);
             //집 모양 만들기
             //지붕
             Gdiplus::Point* RoofPoints = new Gdiplus::Point[3]
             {
-                Gdiplus::Point(200, 100),
-                Gdiplus::Point(120, 160),
-                Gdiplus::Point(280, 160)
+                Gdiplus::Point(HouseStartPoint.X + RoofWidth / 2, HouseStartPoint.Y),           //꼭대기
+                Gdiplus::Point(HouseStartPoint.X, HouseStartPoint.Y + RoofHeight),              //좌측
+                Gdiplus::Point(HouseStartPoint.X + RoofWidth, HouseStartPoint.Y + RoofHeight)   //우측
             };
             GraphicsInstance.FillPolygon(&BlueBrush, RoofPoints, 3);
 
             Gdiplus::Point* WallPoints = new Gdiplus::Point[4]
             {
-                Gdiplus::Point(140, 160),
-                Gdiplus::Point(140, 220),
-                Gdiplus::Point(260, 220),
-                Gdiplus::Point(260, 160)
+                Gdiplus::Point(HouseStartPoint.X + (RoofWidth - WallWidth) / 2, HouseStartPoint.Y + RoofHeight),
+                Gdiplus::Point(HouseStartPoint.X + (RoofWidth - WallWidth) / 2, HouseStartPoint.Y + RoofHeight + WallHeight),
+                Gdiplus::Point(HouseStartPoint.X + RoofWidth - (RoofWidth - WallWidth) / 2, HouseStartPoint.Y + RoofHeight + WallHeight),
+                Gdiplus::Point(HouseStartPoint.X + RoofWidth - (RoofWidth - WallWidth) / 2, HouseStartPoint.Y + RoofHeight)
             };
             
-            //Gdiplus::Rect WallRect(140, 160, 120, 80);
-            //GraphicsInstance.DrawRectangle(&BlackPen, WallRect);
             GraphicsInstance.DrawLines(&BlackPen, WallPoints, 4);
 
             EndPaint(hWnd, &ps);
@@ -207,18 +263,76 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         switch (wParam)
         {
             //VK : Virtual Key            
+        case VK_UP:
+            OnKeyUpForward(hWnd, VK_UP, HouseStartPoint.Y);
+            //0번 자리가 0이면
+            if ((KEY_DOWN & (1 << 1)) != (1 << 1))
+            {
+                KEY_DOWN |= 1 << 1;
+                if (HouseStartPoint.Y > 0)
+                    HouseStartPoint.Y -= 5;
+
+                InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            }
+            break;
+        case VK_DOWN:
+            if ((KEY_DOWN & (1 << 2)) != (1 << 2))
+            {
+                KEY_DOWN |= 1 << 2;
+
+                if (HouseStartPoint.Y < WindowHeight - (RoofHeight + WallHeight + TitleBarHeight))
+                    HouseStartPoint.Y += 5;
+                OutputDebugStringA((std::to_string(HouseStartPoint.Y) + ", " + std::to_string(Limit)).c_str());
+                OutputDebugStringA("\n");
+
+                InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            }
+            
+            break;
         case VK_LEFT:
-            OutputDebugStringW(L"왼쪽키를 눌렀다.\n");
-            InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            if ((KEY_DOWN & (1 << 2)) != (1 << 3))
+            {
+                if (HouseStartPoint.X > 0)
+                    HouseStartPoint.X -= 5;
+                InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            }
+            KEY_DOWN |= 1 << 3;
+
             break;
         case VK_RIGHT:
-            OutputDebugStringW(L"오른쪽키를 눌렀다.\n");
-            InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            if ((KEY_DOWN & (1 << 4)) != (1 << 4))
+            {
+                KEY_DOWN |= 1 << 4;
+
+                if (HouseStartPoint.X < WindowWidth - RoofWidth)
+                    HouseStartPoint.X += 5;
+
+                InvalidateRect(hWnd, nullptr, TRUE);        //창을 다시 그려줌(WM_PAINT 메시지 처리)
+            }
             break;
         case VK_ESCAPE:
             DestroyWindow(hWnd);
         }
     }       
+        break;
+    case WM_KEYUP:
+    {
+        switch (wParam)
+        {
+        case VK_UP:
+            KEY_DOWN &= ~(1 << 1);
+            break;
+        case VK_DOWN:
+            KEY_DOWN &= ~(1 << 2);
+            break;
+        case VK_LEFT:
+            KEY_DOWN &= ~(1 << 3); 
+            break;
+        case VK_RIGHT:
+            KEY_DOWN &= ~(1 << 4);
+            break;
+        }
+    }
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
